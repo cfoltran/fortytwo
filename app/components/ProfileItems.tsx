687a7d7260cc65e505e835a477da6215b7eb1f0a
Ext-Foltran <EXT-Clement.Foltran@crm.microport.com>
2022-11-06 23:14:49 +0100

    Add images to profile items

--- app/components/ProfileItems.tsx
+++ app/components/ProfileItems.tsx
@@ -1,17 +1,21 @@
-import { Text, View, Button, ScrollView } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import React from 'react';
 
 const SearchItems = ({ users }) => {
   return (
-    <View style={{ flex: 1, backgroundColor: '#fff', alignItems: 'flex-end' }}>
-      {
-          users?.map(user => {
-            return (
-              <Text>{user.login}</Text>
-            );
-          })
-      }
-    </View>
+    users?.map(user => {
+      return (
+        <View style={{ flex: 1, flexDirection: 'row' }}>
+          <Image
+            source={{ uri: user.image.versions.small }}
+            style={{ width: 40, height: 40 }}
+          />
+          <Text style={{ padding: 15 }}>
+            {user.login}
+          </Text>
+        </View>
+      );
+    })
   )
 }
 
