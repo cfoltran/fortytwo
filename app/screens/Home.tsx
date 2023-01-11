cc7ecee2d3745f4d2341558ef2472a1e0e01226c
cfoltran <clfoltra@student.42.fr>
2023-01-11 23:09:19 +0100

    create profile content component

--- app/screens/Home.tsx
+++ app/screens/Home.tsx
@@ -1,9 +1,9 @@
 import { View, Button } from 'react-native';
 import React from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import ProfileContent from '../components/ProfileContent';
 
 const Home = ({ navigation, route }) => {
-  navigation.replace('Profile', { userId: 40591 });
   return (
     <View style={{ flex: 1, backgroundColor: '#fff', alignItems: 'flex-end' }}>
       <Button
@@ -14,6 +14,7 @@ const Home = ({ navigation, route }) => {
          }
         }
       />
+      <ProfileContent userId={40591} />
     </View>
   );
 }
